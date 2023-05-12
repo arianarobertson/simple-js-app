cc90052c007eb5293e4d5510a6b1202c05776075
arianarobertson <130882991+arianarobertson@users.noreply.github.com>
2023-05-11 21:06:41 -0700

    Added a css file, basic index file with linked js, a blank readme

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+alert('Hello world');
+
+let age = 29;
+let name = 'Anne';
+
+let myName = 'Bob';
+document.write(myName);
+myName = 'John Doe';
+document.write(myName);
+
+let size = 100;
+let doubleSize = size * 2;
+let minSize = (doubleSize * 2) - (size / 2);
+document.write(minSize);
+
+let favoriteFood = pasta;
+document.write(favoriteFood);
