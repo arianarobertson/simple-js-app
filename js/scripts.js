49fb89160d1df1679b81c58f75b5e3eeddbae07d
arianarobertson <130882991+arianarobertson@users.noreply.github.com>
2023-06-12 23:22:24 -0700

    Added a for loop
    Added a for loop to iterate over each item in my pokemon list.
    
    Added a document.write field to display the pokemon names on my website's DOM

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,8 @@
-let pokemonList = [{name: "Charmander", height: "0.6 meters", type: "fire"}, {name: "Golbat", height: "1.6 meters", type: ["poison", "flying"]}, {name: "Rhydon", height: "1.9 meters", tyoe: ["rock", "ground"]}]
+let pokemonList = [{name: "Charmander", height: "0.6", type: "fire"}, {name: "Golbat", height: "1.6", type: ["poison", "flying"]}, {name: "Rhydon", height: "1.9", tyoe: ["rock", "ground"]}]
+let text = "";
+let i = 0;
+for (;pokemonList;){
+    text = text + " " + pokemonList[i];
+    i++;
+document.write(pokemonList.name);
+}
