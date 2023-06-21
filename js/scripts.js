ed1291fe73e483ee791d630c0647ba5fc66a7d6f
arianarobertson <130882991+arianarobertson@users.noreply.github.com>
2023-06-20 19:12:06 -0700

    fixed my for loop and added text on my DOM
    I edited my for loop to iterate over each pokemonList item correctly.
    
    Added a document.write function that correctly prints the pokemon name and height onto my DOM and added a line break in between each output

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,30 @@
-let pokemonList = [{name: "Charmander", height: "0.6", type: "fire"}, {name: "Golbat", height: "1.6", type: ["poison", "flying"]}, {name: "Rhydon", height: "1.9", tyoe: ["rock", "ground"]}]
-let text = "";
-let i = 0;
-for (;pokemonList;){
-    text = text + " " + pokemonList[i];
-    i++;
-document.write(pokemonList.name);
+let pokemonList = [
+    {
+        name: "Charmander",
+        height: 0.6,
+        type: ["fire"]
+    },
+    {
+        name: "Golbat",
+        height: 1.6,
+        type: ["poison", "flying"]
+    },
+    {
+        name: "Rhydon",
+        height: 1.9,
+        type: ["rock", "ground"]
+    },
+    ];
+  
+for (let i = 0; i < pokemonList.length; i++) {
+    let pokemon = pokemonList[i];
+    let output = pokemon.name + " (height: " + pokemon.height + ")";
+    
+    // Check if the height is above a certain value and add a special note
+    if (pokemon.height > 1.5) {
+        output += " - Wow, that's big!";
+    }
+    
+    document.write(output + "<br>");
+    document.write("<br>"); // Add a line break between each output
 }
